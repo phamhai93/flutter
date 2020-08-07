b089c6954a4cc12dc98f0097b54e1b70f0948d76
Phạm Hải <>
2020-08-07 18:41:15 +0700

    update folder models,widgets

---
+++ myapp1/lib/widgets/user_transaction.dart
@@ -0,0 +1,42 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import './new_transaction.dart';
+import './transaction_list.dart';
+import '../models/transaction.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', title: 'New Shose', amount: 69.69, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Week Grocerices', amount: 16.69, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTX = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransaction.add(newTX);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransaction)
+      ],
+    );
+  }
+}
